5c1cd67328dfe2eb5d31b0d774f10a1eb89f8be2
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-08-01 16:48:36 +0200

    Styling

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,15 +1,20 @@
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import { PageNames } from '../App'
 import useQuestions from '../hooks/useQuestions'
+import { useState } from 'react'
 
 interface Props {
   setCurrentPage: React.Dispatch<React.SetStateAction<keyof PageNames>>
 }
 
 const Quiz = ({ setCurrentPage }: Props) => {
+  const [allAnswered, setAllAnswered] = useState(false)
   const { data, error, isLoading } = useQuestions()
 
-  const shuffledQuestions = data.map((question) => ({
+  const shuffledQuestions = data?.map((question) => ({
     ...question,
     answers: [question.correct_answer, ...question.incorrect_answers]
   }))
@@ -24,21 +29,33 @@ const Quiz = ({ setCurrentPage }: Props) => {
   if (error) return <p className='text-danger'>{error}</p>
 
   return (
-    <>
+    <Row>
       {shuffledQuestions?.map((question) => (
-        <div className='text-primary' key={question.id}>
-          <h3>{question.question}</h3>
-          {question.answers.map((answer) => (
-            <Button variant='btn btn-outline-primary' key={answer}>
-              {answer}
-            </Button>
-          ))}
-        </div>
+        <Col className='col-12  text-primary border-bottom' key={question.id}>
+          <h3 className='mb-3'>{question.question}</h3>
+          <div className='d-flex flex-column flex-sm-row'>
+            {question?.answers.map((answer) => (
+              <Button
+                className='mb-3 rounded-4 me-5'
+                variant='btn btn-outline-primary'
+                key={answer}
+              >
+                {answer}
+              </Button>
+            ))}
+          </div>
+        </Col>
       ))}
-      <Button onClick={() => setCurrentPage('result')} variant='secondary'>
-        Check Answers
-      </Button>
-    </>
+      {allAnswered && (
+        <Button
+          className='px-5 py-3 rounded-4  mt-4 mx-auto w-50 w-md-25'
+          onClick={() => setCurrentPage('result')}
+          variant='secondary'
+        >
+          Check Answers
+        </Button>
+      )}
+    </Row>
   )
 }
 
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -9,18 +9,18 @@ interface Props {
 
 const Wrapper = ({ children }: Props) => {
   return (
-    <Container className='d-flex d-column align-items-center justify-content-center h-100'>
+    <Container className='d-flex d-column align-items-center justify-content-center py-3 min-vh-100'>
       <img
         className='position-absolute end-0 top-0'
         src={yellowDot}
         alt='yellow dot'
       />
-      <div className='position-relative'>{children}</div>
       <img
         className='position-absolute start-0 bottom-0'
         src={blueDot}
         alt='blue dot'
       />
+      <div className='position-relative'>{children}</div>
     </Container>
   )
 }
